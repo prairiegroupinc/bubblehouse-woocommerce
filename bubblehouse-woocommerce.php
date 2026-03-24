279eb1281d68f0e5f06391ca2604bd083c757521
Stanislav Seletskiy <s.seletskiy@gmail.com>
2026-03-24 18:29:26 +0400

    bump version to 1.2.0

--- bubblehouse-woocommerce.php
+++ bubblehouse-woocommerce.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Bubblehouse WooCommerce Integration
  * Description: Provides iframe block for Bubblehouse integration
- * Version: 1.1.0
+ * Version: 1.2.0
  * Author: Bubblehouse
  */
 
